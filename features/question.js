c45b5809fb37fe9bf33890e471a70695bd584c5a
Glen Takahashi <glen@glentaka.com>
2019-09-12 22:09:01 -0400

    Logging

--- features/question.js
+++ features/question.js
@@ -34,6 +34,7 @@ If you give up just type \`answer\`.`);
         if (threadId != null && threadId in questionCache) {
             const guess = message.text.toLowerCase();
             const answer = questionCache[threadId];
+            console.log(guess, answer, guess == answer.toLowerCase());
             await bot.startConversationInThread(message.channel, 'fake', threadId);
             if (guess == 'answer') {
                 bot.say(answer);
